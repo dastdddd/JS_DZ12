510a355cd37775fc76cfb0d837e1db11a7c5a5cb
dastdddd <dastdddd@gmail.com>
2024-06-28 15:08:17 +0600

    дз5

---
+++ main.js
@@ -0,0 +1,23 @@
+fetch(
+  "https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=REPLACE_ME"
+)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+      // console.log(data[i]);
+      document.querySelector(".images").innerHTML = `
+    <img src="${data[i].url}" alt="">
+  `;
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+      document.querySelector(".images").style.display = "none";
+      document.querySelector(".error").innerHTML = `
+      <h2>Error</h2>
+      <h1>${err}</h1>
+    `;
+  });
